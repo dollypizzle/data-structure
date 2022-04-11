30a728a827427686b144dcade8159c693882f4b7
dollypizzle <dolapo.mabawonku@gmail.com>
2022-04-11 09:13:12 +0100

    Delete a Node Challenge Solved

---
+++ delete-a-node/delete-a-node.js
@@ -0,0 +1,103 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+/*
+    Delete Node at a given position in a linked list
+    head pointer input could be NULL as well for empty list
+    Node is defined as
+    var Node = function(data) {
+        this.data = data;
+        this.next = null;
+    }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function deleteNode(head, position) {
+    if (!head) {
+        return head;
+    }
+    
+    if (position === 0) {
+        return head.next;
+    }
+    
+    let current = head;
+    let next = head.next;
+    let counter = 1;
+    
+    while (next != null && counter < position) {
+        current = current.next;
+        next = next.next;
+        counter++;
+    }
+    
+    if (next) {
+        current.next = next.next;
+    }
+    
+    return head;
+}
+function main() {
